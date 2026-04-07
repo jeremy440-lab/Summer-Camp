2524cafa569dfb2cf77704c7ae9af1257263da7b
jeremy440-lab <jeremy440@gmail.com>
2026-04-06 17:18:37 -0700

    Add files via upload

---
+++ rise/giddy-up-w2/one-question-can-shift-a-room-raw-SHIhOYX5/content/assets/z2uVg-OX0ep-LMh5/story_content/6ikInNcypcG_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/6ikInNcypcG_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind%3A%20captions%0D%0ASource%3A%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version%3A%203.114.36620.0%0D%0A%0D%0A00%3A00%3A00.150%20--%3E%2000%3A00%3A01.802%0D%0AWhat%20did%20you%20almost%20say%20instead%3F%0D%0A%0D%0A"}]})
